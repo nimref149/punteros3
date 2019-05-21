0861c4ac942d709c6b135ef04de9fc54b6004ed8
Cristian olv <47321357+nimref149@users.noreply.github.com>
2019-05-21 16:28:23 -0300

    Add files via upload

--- Memoriadinamicaa/main.c
+++ Memoriadinamicaa/main.c
@@ -8,10 +8,12 @@ int main()
     //pNumeros=(int*) malloc(sizeof(int)*5); pNu me va a devolver 4, le estoy pididendo 20 bytes en memoria de manera consecutiva en el hit.
     int* pNumeros;
     int i;
-    int* pAux;
-
-    pAux=(int*) malloc(sizeof(int)*(T+5));
+    int* aux;
     pNumeros=(int*) malloc(sizeof(int)*T);
+
+
+
+
 
     if(pNumeros!=NULL)
     {
@@ -26,43 +28,96 @@ int main()
             printf("%d\n",*(pNumeros+i));
         }
 
+        aux=(int*)realloc(pNumeros,sizeof(int)*(T+5));
 
-
-    }
-
-    pAux=(int*)realloc(pNumeros,sizeof(int)*(T+5));
-
-    if(pAux!=NULL){
-
-        for(i=0; i<T+5; i++)
+        if(aux!=NULL)
         {
-            printf("%d\n",*(pAux+i));
+            pNumeros=aux;
+            for(i=T; i<T+5; i++)
+            {
+                printf("Ingrese un numero: ");
+                scanf("%d",pNumeros+i);
+            }
+            for(i=0; i<T+5; i++)
+            {
+                printf("%d\n",*(pNumeros+i));
+            }
+            pNumeros=(int*)realloc(pNumeros,sizeof(int)*4);//Lo achica los lugares de pnumeros hasta 4 y muestra valores basura despues del 4 lugar
+            printf("Luego de achicar\n");//muestra todos los valores pero desdepues del cuarto lugar es basura
+            for(i=0;i<T+5;i++){
+                printf("%d\n",*(pNumeros+i));
+
+            }
+        }
+        else
+        {
+            printf("no hay memoria suficiente!!!");
         }
 
     }
 
 
 
+    /*
 
 
+    if(pNumeros!=NULL)
+    {
+        for(i=0; i<T; i++)
+        {
+            *(pNumeros+i)=i+1;
+            printf("Ingrese un numero: ");
+            scanf("%d",pNumeros+i);
+        }
+        for(i=0; i<T; i++)
+        {
+            printf("%d\n",*(pNumeros+i));
+        }
+
+        aux=(int*)realloc(pNumeros,sizeof(int)*(T+5));//COPIA LO QUE ESTA EN PNUMEROS a un auxiliar Y EXTIENDE EL LUGAR(SON 5) HASTA 10 LUGARES, EN CASO DE QUE NO PUEDA DAR 10 LUGARES LIBRES(EXTENDER 5 MAS) CONSECUTIVOS SE VUELVE NULL el auxiliar
+        if(aux!=NULL)
+        {
+            pNumeros=aux;
+            for(i=T;i<T+5;i++)
+                {
+            printf("Ingrese un numero: ");
+            scanf("%d",pNumeros+i);
+            }
+            for(i=0;i<T+5;i++)
+            {
+                printf("%d\n",*(pNumeros+i));
+            }
+
+        }
+        else
+        {
+            printf("no hay memoria suficiente!!!");
+        }
+
+    }
+
+   */
+
+
+//-------------
 
 
 /*
 
-int* pNumeros;
-        int i;
-        pNumeros = (int*) malloc(sizeof(int)*5);
-        if(pNumeros!=NULL)
+    int* pNumeros;
+    int i;
+    pNumeros = (int*) malloc(sizeof(int)*5);
+    if(pNumeros!=NULL)
+    {
+        for(i=0; i<5; i++)
         {
-            for(i=0;i<5;i++)
-            {
-                *(pNumeros+i)=i+1;
-            }
-            for(i=0;i<5;i++)
-            {
-                printf("%d\n",*(pNumeros+i));
-            }
+            *(pNumeros+i)=i+1;
         }
+        for(i=0; i<5; i++)
+        {
+            printf("%d\n",*(pNumeros+i));
+        }
+    }
 
 
 
@@ -71,8 +126,7 @@ int* pNumeros;
 
 
 
-
-//-_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_-
+//------------
 
 /*
 
@@ -82,17 +136,18 @@ int* pNumeros;
     printf("&Stack->&pNumero: %d\n",&pNumero);
 
 
-    if(pNumero!=NULL){
+    if(pNumero!=NULL)
+    {
 
-    printf("Valor->*pNumero: %d\n",*pNumero);
-    system("pause");
-    printf("Luego de hacer un free: \n");
-    free(pNumero);
-    printf("Heap->pNumero: %d\n",pNumero);
-    printf("Stack ->&pNumero: %d\n",&pNumero);
-    printf("Valor->*pNumero: %d\n",*pNumero);
-*/
-//-_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_-
+        printf("Valor->*pNumero: %d\n",*pNumero);
+        system("pause");
+        printf("Luego de hacer un free: \n");
+        free(pNumero);
+        printf("Heap->pNumero: %d\n",pNumero);
+        printf("Stack ->&pNumero: %d\n",&pNumero);
+        printf("Valor->*pNumero: %d\n",*pNumero);
+        */
+
 
     return 0;
 }
